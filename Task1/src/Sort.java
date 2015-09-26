3272fb27f358e69c849e8f63d6bdb969172c52f1
Ekaterina Strog <katya_strog@mail.ru>
2015-09-26 23:32:12 +0400

    new sort

--- Task1/src/Sort.java
+++ Task1/src/Sort.java
@@ -5,35 +5,33 @@
  */
 
 public class Sort {
-    static void SelectionSort (double [] arr) {
-        int i, j, indexOfMinElem;
-        double tmp;
+    static void selectionSort(double[] arr) {
 
-        for (i = 0; i < arr.length - 1; ++i) {
-            indexOfMinElem = i;
-            for (j = i + 1; j < arr.length; ++j) {
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int indexOfMinElem = i;
+            for (int j = i + 1; j < arr.length; ++j) {
                 if (arr[j] < arr[indexOfMinElem]) {
                     indexOfMinElem = j;
                 }
             }
             if (indexOfMinElem != i) {
-                tmp = arr[i];
+                double tmp = arr[i];
                 arr[i] = arr[indexOfMinElem];
                 arr[indexOfMinElem] = tmp;
             }
         }
     }
-    public static void main(String[] args) {
-        int i;
-        double [] arr = new double [args.length];
 
-        for (i = 0; i < args.length; ++i) {
+    public static void main(String[] args) {
+        double[] arr = new double[args.length];
+
+        for (int i = 0; i < args.length; ++i) {
             arr[i] = Double.parseDouble(args[i]);
         }
 
-        SelectionSort(arr);
+        selectionSort(arr);
 
-        for (i = 0; i < args.length; ++i) {
+        for (int i = 0; i < args.length; ++i) {
             System.out.print(arr[i] + " ");
         }
     }
