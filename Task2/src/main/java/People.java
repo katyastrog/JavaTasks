4f263ff4eec53474ee0555cf2ba2b8b0a2708db0
Ekaterina Strog <katya_strog@mail.ru>
2015-09-30 01:22:44 +0400

    People, SelectionSort and Sort classes have been added

---
+++ Task2/src/main/java/People.java
@@ -0,0 +1,24 @@
+
+/**
+ * Created by Екатерина on 26.09.2015.
+ */
+
+import java.util.Comparator;
+
+public class People {
+    private final int age;
+    private final String name;
+
+    public People(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Task2/src/main/java/Sort.java
@@ -0,0 +1,8 @@
+
+import java.util.Comparator;
+
+public interface Sort<T> {
+    T[] sort(T[] array, Comparator<T> comparator);
+
+}
+
