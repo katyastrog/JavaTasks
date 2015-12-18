81982d8b2ee084262ba64f6d51bd7356898c5a71
Gleb Azeev <azeevg@gmail.com>
2015-12-19 01:28:54 +0300

    new class Expression contains expression

---
+++ Task4/src/main/java/ru/spbstu/appmath/strogalshchikova/Expression.java
@@ -0,0 +1,25 @@
+package ru.spbstu.appmath.strogalshchikova;
+
+
+import ru.spbstu.appmath.strogalshchikova.exceptions.*;
+
+import java.util.List;
+
+public class Expression {
+
+    private final List<Lexeme> expression;
+    private final boolean isVarExpected;
+
+    public Expression(final String input) throws UnhandledLexemeException, WrongSyntaxException, ParenthesesBalanceException, TooManyVariablesException {
+        expression = Parser.parse(input);
+        isVarExpected = Analyzer.analyze(expression);
+    }
+
+    public List<Lexeme> getExpression() {
+        return expression;
+    }
+
+    public boolean isVarExpected() {
+        return isVarExpected;
+    }
+}
