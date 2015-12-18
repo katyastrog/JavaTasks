19fede5e694b493cc20df0df0b368e05232e71da
Gleb Azeev <azeevg@gmail.com>
2015-12-19 02:05:55 +0300

    ExpressionTree class was added

---
+++ Task4/src/main/java/ru/spbstu/appmath/strogalshchikova/ExpressionTree.java
@@ -0,0 +1,20 @@
+package ru.spbstu.appmath.strogalshchikova;
+
+
+import ru.spbstu.appmath.strogalshchikova.exceptions.DivisionByZeroException;
+
+import java.util.List;
+
+public class ExpressionTree {
+
+    public ExpressionTree(final List<Lexeme> expression) {
+
+    }
+
+
+    public String calculate() throws DivisionByZeroException {
+    }
+
+    public String calculate(final double realValue) throws DivisionByZeroException {
+    }
+}
