a233e3a81912d4609fdbba775787f90b61950b02
Katya Strog <katya_strog@mail.ru>
2015-12-25 21:23:05 +0300

    Project was replaced

--- Task4/src/main/java/ru/spbstu/appmath/strogalshchikova/Lexeme.java
+++ Task4/src/main/java/ru/spbstu/appmath/strogalshchikova/Lexeme.java
@@ -7,14 +7,33 @@ public class Lexeme {
     private final LexemeType type;
     private final String value;
 
-    public Lexeme(final LexemeType type, final String value) {
-        this.type = type;
+    public Lexeme(final String value) throws UnhandledLexemeException {
+        this.type = classify(value);
         this.value = value;
     }
 
-    public Lexeme(final String value) throws UnhandledLexemeException {
-        this.type = LexemeClassifier.classify(value);
-        this.value = value;
+    private static LexemeType classify(final String lexeme) throws UnhandledLexemeException {
+        if (lexeme.length() == 0)
+            throw new IllegalArgumentException();
+
+        //final String brackets = "()";
+        final String operands = "+-*/";
+
+        if (lexeme.matches(Parser.RE_NUM)) { // numbers
+            return LexemeType.NUMBER;
+        } else if (operands.contains(lexeme) && lexeme.length() == 1) { // operands
+            return LexemeType.OPERAND;
+        } else if (lexeme.equals("(")) { // left bracket
+            return LexemeType.LEFT_BRACKET;
+        } else if (lexeme.equals(")")) { // right bracket
+            return LexemeType.RIGHT_BRACKET;
+        } else if (lexeme.matches(Parser.RE_LETTERS)) { // variables and constants
+            return LexemeType.VARIABLE;
+        } else if (lexeme.matches(Parser.RE_RANGE)) { // variable range
+            return LexemeType.RANGE;
+        } else {
+            throw new UnhandledLexemeException();
+        }
     }
 
     public Double getRealValue() {
